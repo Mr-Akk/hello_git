f633a31f9bb873e5c8831b776f0c16c3db6f74d0
Akash Mohan <akm99spl@gmail.com>
2021-01-06 20:19:10 +0530

    commit-2 with hello.java modified

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ package hello;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
+		System.out.println("Change after first commit");
 		
 
 	
